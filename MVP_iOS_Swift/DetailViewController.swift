346899d836416b865f4078f25e2263c8e6e32d89
Raj Maurya <rajmaurya8085@gmail.com>
2019-09-19 09:49:03 +0530

    Initial Commit

---
+++ MVP_iOS_Swift/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  MVP_iOS_Swift
+//
+//  Created by Raj Maurya on 19/09/19.
+//  Copyright © 2019 Raj Maurya. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
